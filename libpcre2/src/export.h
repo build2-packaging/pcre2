7cc51cba9c8b5690280e4afd9d067e284fe29a9c
Francois Kritzinger <francois@codesynthesis.com>
2021-10-08 16:26:56 +0200

    Create libpcre2 build2 package

---
+++ libpcre2/src/export.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#if defined(SRC_STATIC)         /* Using static. */
+#  define SRC_SYMEXPORT
+#elif defined(SRC_STATIC_BUILD) /* Building static. */
+#  define SRC_SYMEXPORT
+#elif defined(SRC_SHARED)       /* Using shared. */
+#  ifdef _WIN32
+#    define SRC_SYMEXPORT __declspec(dllimport)
+#  else
+#    define SRC_SYMEXPORT
+#  endif
+#elif defined(SRC_SHARED_BUILD) /* Building shared. */
+#  ifdef _WIN32
+#    define SRC_SYMEXPORT __declspec(dllexport)
+#  else
+#    define SRC_SYMEXPORT
+#  endif
+#else
+/* If none of the above macros are defined, then we assume we are being used
+ * by some third-party build system that cannot/doesn't signal the library
+ * type. Note that this fallback works for both static and shared libraries
+ * provided the library only exports functions (in other words, no global
+ * exported data) and for the shared case the result will be sub-optimal
+ * compared to having dllimport. If, however, your library does export data,
+ * then you will probably want to replace the fallback with the (commented
+ * out) error since it won't work for the shared case.
+ */
+#  define SRC_SYMEXPORT         /* Using static or shared. */
+/* #  error define SRC_STATIC or SRC_SHARED preprocessor macro to signal libpcre2 library type being linked */
+#endif
